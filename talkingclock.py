a1664cae2ce2af2fab5e3b79b69a29fec0c30c30
Jeffrey Wang <66625372+JeffreyWangDev@users.noreply.github.com>
2023-08-04 09:33:11 -0400

    asd

--- talkingclock.py
+++ talkingclock.py
@@ -24,15 +24,12 @@ Input: 23:59  Output: It's eleven fifty nine pm
 Input: 12:05  Output: It's twelve oh five pm
 """
 
-
-hours = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 0:"twelve"}
-minutes = {1:"oh one",2:"oh two",3:"oh three",4:"oh four",5:"oh five",10:"ten",11:"eleven",13:"thirteen",14:"fourteen",19:"nineteen",20:"twenty",30:"thirty",29:"twenty nine",39:"thirty nine",49:"forty nine",59:"fifty nine"}
 # This will convert military hours to regular hours, and determine AM vs PM
 
 reg_tens = {2:"twenty",3:"thirty",4:"forty",5:"fifty"}
 teens = {10:"ten",11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
 ones = {1:"oh one","2":"oh two","3":"oh three","4":"oh four","5":"oh five","6":"oh six","7":"oh seven","8":"oh eight","9":"oh nine"}
-reg_ones = {1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
+reg_ones = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 0:"twelve"}
 
 class Solution:    
     def ClockTalker(self, input_time):
@@ -45,7 +42,7 @@ class Solution:
             hour = int(input_time.split(":")[0]) - 12
         minutes = int(input_time.split(":")[1])
         end_str += "It's "
-        end_str += hours[hour]
+        end_str += reg_ones[hour]
         if minutes == 0:
             end_str += " " + a
             return end_str
